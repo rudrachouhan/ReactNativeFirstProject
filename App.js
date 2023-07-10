244d91f1f6f2e81d1cadfd7904a3660c3ae852aa
rudrachouhan <rudra678chouhan@gmail.com>
2023-07-10 20:02:25 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, FlatList, StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleAddTask = () => {
+    if (task !== '') {
+      setTaskList([...taskList, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedList = [...taskList];
+    updatedList.splice(index, 1);
+    setTaskList(updatedList);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.heading}>Tasker</Text>
+        <Text style={styles.des}>My Todos:-</Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter a task"
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <Button title="Add" onPress={handleAddTask} />
+      </View>
+      <FlatList
+        data={taskList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.taskContainer}>
+            <Text style={styles.task}>{item}</Text>
+            <Button title="Delete" onPress={() => handleDeleteTask(index)} />
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+
+  header:{
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight:'bold'
+  },
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 10,
+    padding: 10,
+  },
+  taskContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  task: {
+    flex: 1,
+    marginRight: 10,
+  },
+  des: {
+    color: 'dodgerblue',
+    fontSize:24,
+  }
 });
+
